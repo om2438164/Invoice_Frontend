5e393062ee22df6a3896881a242db99d4853cc09
ompatil <om2438164@gmail.com>
2025-06-24 21:11:44 +0530

    commited

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'http://localhost:8080/api';
 
 // Create axios instance with default config
 const api = axios.create({
