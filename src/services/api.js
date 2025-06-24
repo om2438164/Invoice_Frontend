23977186f083b8229b32c7780ad39d26f0148860
ompatil <om2438164@gmail.com>
2025-06-24 14:18:12 +0530

    added

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'endearing-ambition-production.up.railway.app';
 
 // Create axios instance with default config
 const api = axios.create({
